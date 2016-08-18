5e66588b5ba6a4e269a33a82041502b28a14b595
Pablo Barttfeld <cabalangofeld@gmail.com>
2016-08-18 19:20:05 -0300

    movi parametros de trials a parameters.js

--- js/parameters.js
+++ js/parameters.js
@@ -13,3 +13,10 @@ sc.nstim        	= [];		// number of items to display
 sc.stimvalues   	= [];		// for the area differences to be stored
 sc.tomultiply   	= [];		// to multiply stimval for Area3
 sc.whichisbigger	= [];		// bigger the square or the circle
+
+
+
+var numberofstimuli     = [2, 3];                 // number of items to display each trial
+var areavalues          = [0.9, .7, .6, .5];      // area difference between figures. area2 = area1 * areavalues[i] 
+var bigger              = [1, 2];                 // bigger the square or the circle
+var areaforthethirdone  = [0.6, 0.35, 0.1];       // area difference between figures. area3 = area2 * areavalues[i]
